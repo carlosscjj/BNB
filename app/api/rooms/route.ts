0a6261a2650c4a9b0072f748eaad895d82cd5d86
carlos <carlosalmeidagomes12@gmail.com>
2026-02-16 18:26:42 +0000

    debug cloudinary env

--- app/api/rooms/route.ts
+++ app/api/rooms/route.ts
@@ -4,6 +4,9 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { v2 as cloudinary } from "cloudinary";
 
+console.log("CLOUD_NAME:", process.env.CLOUDINARY_CLOUD_NAME);
+console.log("API_KEY:", process.env.CLOUDINARY_API_KEY);
+console.log("API_SECRET:", process.env.CLOUDINARY_API_SECRET);
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
