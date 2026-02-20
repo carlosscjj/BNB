8d8eb4d93b332c2a348ef7d8c1b461bff2f28057
carlos <carlosalmeidagomes12@gmail.com>
2026-02-20 17:47:28 +0000

    Criação da rota protegida admin/users/new e remoção de possíveis duplicatas de admin fora da pasta (protected). Limpeza da pasta .next.

---
+++ app/(protected)/admin/users/new/page.tsx
@@ -0,0 +1,9 @@
+// Página de criação de novo usuário
+export default function NewUserPage() {
+  return (
+    <div>
+      <h1>Criar Novo Usuário</h1>
+      {/* Adicione aqui o formulário de criação de usuário */}
+    </div>
+  );
+}
